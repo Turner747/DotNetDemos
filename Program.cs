98b846e8e92c48fe5613c3cb37714ba21ec0a2a9
Joshua Turner <joshua.turner2021@gmail.com>
2022-07-29 23:12:12 +1000

    new exercises, updated read me

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UdemyDemos
 {
@@ -16,7 +19,566 @@ namespace UdemyDemos
 
         public static void Main(string[] args)
         {
-            LoopingMatrix();
+            OrderDemo();
+        }
+
+        public static void OrderDemo()
+        {
+            Queue<Order> orderQueue = new Queue<Order>();
+
+            foreach(Order o in ReceiveOrdersFromBranchOne())
+            {
+                orderQueue.Enqueue(o);
+            }
+
+            foreach (Order o in ReceiveOrdersFromBranchTwo())
+            {
+                orderQueue.Enqueue(o);
+            }
+
+            while (orderQueue.Count > 0)
+            {
+                Order currentOrder = orderQueue.Dequeue();
+                currentOrder.ProcessOrder();
+            }
+        }
+        
+        public static void QueueDemo()
+        {
+            Queue<int> q = new Queue<int>();
+
+            q.Enqueue(1);
+            Console.WriteLine("Item at the start of the queue: {0}", q.Peek());
+            q.Enqueue(2);
+            Console.WriteLine("Item at the start of the queue: {0}", q.Peek());
+            q.Enqueue(3);
+            Console.WriteLine("Item at the start of the queue: {0}", q.Peek());
+
+            int item = q.Dequeue();
+            Console.WriteLine("Item at the start of the queue: {0}", q.Peek());
+
+            q.Enqueue(4);
+            q.Enqueue(5);
+
+            while (q.Count > 0)
+            {
+                Console.WriteLine("Item {0} being processed has been removed from the queue", q.Peek());
+                item = q.Dequeue();
+            }
+        }
+
+        class Order
+        {
+            public int OrderID { get; set; }
+            public int OrderQuantity { get; set; }
+
+            public Order(int orderID, int orderQuantity)
+            {
+                this.OrderID = orderID;
+                this.OrderQuantity = orderQuantity;
+            }
+
+            public void ProcessOrder()
+            {
+                Console.WriteLine($"Order {OrderID} processed!");
+            }
+        }
+
+        static Order[] ReceiveOrdersFromBranchOne()
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(1,5),
+                new Order(2,4),
+                new Order(6,10)
+            };
+            return orders;
+        }
+        static Order[] ReceiveOrdersFromBranchTwo()
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(3,5),
+                new Order(4,4),
+                new Order(5,10)
+            };
+            return orders;
+        }
+
+        public static void StackDemo()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(1);
+            Console.WriteLine("Item at the top of the stack: {0}", stack.Peek());
+            stack.Push(2);
+            Console.WriteLine("Item at the top of the stack: {0}", stack.Peek());
+            stack.Push(3);
+            Console.WriteLine("Item at the top of the stack: {0}", stack.Peek());
+            stack.Push(4);
+            Console.WriteLine("Item at the top of the stack: {0}", stack.Peek());
+            stack.Push(5);
+            Console.WriteLine("Item at the top of the stack: {0}", stack.Peek());
+
+            int poppedItem = stack.Pop();
+            
+            Console.WriteLine("Popped item: {0}", poppedItem);
+
+            Console.WriteLine("Item at the top of the stack: {0}", stack.Peek());
+            
+            while(stack.Count > 0)
+            {
+                Console.WriteLine("The value {0}, at the top was removed", stack.Pop());
+                Console.WriteLine("There are now {0} items left in the stack", stack.Count);
+            }
+
+            int[] numbers = new int[] { 8, 2, 3, 4, 7, 6, 2 };
+
+            Stack<int> numStack = new Stack<int>();
+
+            foreach(int num in numbers)
+            {
+                Console.Write(num + " ");
+
+                numStack.Push(num);
+            }
+
+            Console.WriteLine();
+
+            while(numStack.Count > 0)
+            {
+                int tempNum = numStack.Pop();
+                
+                Console.Write(tempNum + " ");
+            }
+        }
+
+        public static void DictionaryExamples()
+        {
+            Employee[] employees =
+            {
+                new Employee("CEO", "Gwyn", 95,200),
+                new Employee("Manager", "Joe", 35,25),
+                new Employee("HR", "Lora", 32,21),
+                new Employee("Secretary", "Petra", 28,18),
+                new Employee("Lead Developer", "Artorias", 55,35),
+                new Employee("Intern", "Ernest", 22,8)
+            };
+
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>()
+            {
+                {1, "One"},
+                {2, "Two"},
+                {3, "Three"}
+            };
+
+            Dictionary<string, Employee> employeesDirectory = new Dictionary<string, Employee>();
+            foreach(Employee emp in employees)
+            {
+                employeesDirectory.Add(emp.Role, emp);
+            }
+
+            string key = "CEO";
+
+            if (employeesDirectory.ContainsKey(key))
+            {
+                Employee empl = employeesDirectory[key];
+                Console.WriteLine("Emploee name: {0}, Age: {1}, Role: {2}, Salary: {3}", empl.Name, empl.Age, empl.Role, empl.Salary);
+            }
+            else
+            {
+                Console.WriteLine("No employe was found with role: {0}", key);
+            }
+
+            Employee result = null;
+
+            string key2 = "Intern";
+
+            if (employeesDirectory.TryGetValue(key2, out result))
+            {
+                Console.WriteLine("\nValue retrieved!\n");
+
+                Console.WriteLine("Emploee name: {0}", result.Name);
+                Console.WriteLine("Age: {0}", result.Age);
+                Console.WriteLine("Role: {0}", result.Role);
+                Console.WriteLine("Salary: {0}", result.Salary);
+            }
+            else
+            {
+                Console.WriteLine("No employe was found with role: {0}", key2);
+            }
+
+            Console.WriteLine();
+
+            string keyToUpdate = "HR";
+            if (employeesDirectory.ContainsKey(keyToUpdate))
+            {
+                employeesDirectory[keyToUpdate] = new Employee("HR", "Eleka", 26, 18);
+                Console.WriteLine("Employee with role/key {0} was updated!", keyToUpdate);
+            }
+
+            Console.WriteLine();
+
+            string keyToRemove = "Intern";
+            if (employeesDirectory.Remove(keyToRemove))
+            {
+                Console.WriteLine("Employee with role/key {0} was removed!", keyToRemove);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < employeesDirectory.Count; i++)
+            {
+                KeyValuePair<string, Employee> keyValuePair = employeesDirectory.ElementAt(i);
+
+                Console.WriteLine("Key: {0}, {1}", keyValuePair.Key, i);
+
+                Employee employeeValue = keyValuePair.Value;
+
+                Console.WriteLine("Emploee name: {0}", employeeValue.Name);
+                Console.WriteLine("Employee Age: {0}", employeeValue.Age);
+                Console.WriteLine("Employee Role: {0}", employeeValue.Role);
+                Console.WriteLine("Employee Salary: {0}", employeeValue.Salary);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(ConvertNumber(3)); 
+        }
+        
+        public static string ConvertNumber(int i)
+        {
+            Dictionary<int, string> numbers = new Dictionary<int, string>() {
+                {1, "one"},
+                {2, "two"},
+                {3, "three"},
+                {4, "four"},
+                {5, "five"}
+            };
+
+            if(numbers.TryGetValue(i, out string value))
+                return value;
+            else
+                return "nope";
+        }
+
+        class Employee
+        {
+            public string Role { get; set; }
+            public string Name { get; set; }
+            public  int Age { get; set; }
+            public float Rate { get; set; } = 20.50f;
+            public float Salary 
+            {
+                get
+                {
+                    return ((Rate * 8) * 5) * 52;
+                } 
+            }
+
+            public Employee(string role, string name, int age, float rate)
+            {
+                this.Role = role;
+                this.Name = name;
+                this.Age = age;
+                this.Rate = rate;
+            }
+        }
+
+        public static void HashtableChallenge()
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student(1, "Denis", 88);
+            students[1] = new Student(2, "Olaf", 97);
+            students[2] = new Student(6 ,"Ragner", 65);
+            students[3] = new Student(1, "Luise", 73);
+            students[4] = new Student(4, "Levi", 58);
+
+            Hashtable studentsTable = new Hashtable();
+
+            foreach (Student stud in students)
+            {
+                if (!studentsTable.ContainsKey(stud.Id))
+                {
+                    studentsTable.Add(stud.Id, stud);
+                    Console.WriteLine("Student with ID: {0}, added", stud.Id);
+                }  
+                else
+                    Console.WriteLine("Sorry a student already exist with ID: {0}", stud.Id);
+            }
+        }
+
+        public static void HashtableExamples()
+        {
+            Hashtable studentTable = new Hashtable();
+
+            Student stud1 = new Student(1, "Josh", 7f);
+            Student stud2 = new Student(2, "Jason", 3.33f);
+            Student stud3 = new Student(3, "Claire", 4.4f);
+            Student stud4 = new Student(4, "Tim", 5.5f);
+            Student stud5 = new Student(5, "Anna", 6.1f);
+
+            studentTable.Add(stud1.Id, stud1);
+            studentTable.Add(stud2.Id, stud2);
+            studentTable.Add(stud3.Id, stud3);
+            studentTable.Add(stud4.Id, stud4);
+            studentTable.Add(stud5.Id, stud5);
+            
+            Student storedStudent1 = (Student) studentTable[4];
+
+            Console.WriteLine("Student ID: {0}, Name: {1}, GPA: {2}", storedStudent1.Id, storedStudent1.Name, storedStudent1.GPA);
+
+            Console.WriteLine();
+
+            foreach (DictionaryEntry student in studentTable)
+            {
+                Student storedStudent = (Student)student.Value;
+                Console.WriteLine("Student ID: {0}, Name: {1}, GPA: {2}", storedStudent.Id, storedStudent.Name, storedStudent.GPA);
+            }
+
+            Console.WriteLine();
+            
+            foreach(Student stud in studentTable.Values)
+            {
+                Console.WriteLine("Student ID: {0}, Name: {1}, GPA: {2}", stud.Id, stud.Name, stud.GPA);
+            }
+        }
+
+        class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public float GPA { get; set; }
+
+            public Student(int id, string name, float gpa)
+            {
+                this.Id = id;
+                this.Name = name;
+                this.GPA = gpa;
+            }
+        }
+
+        public static void CollectionListExamples()
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 100; i <= 170; i+=2)
+            {
+                list.Add(i);
+            }
+
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static void CollectionArrayListExamples()
+        {
+            // declaring
+            ArrayList myArrList = new ArrayList();      // undefined num of objects
+            ArrayList myArrList2 = new ArrayList(100);  //defined num of objects
+
+            // add elements
+            myArrList.Add("Start");
+            myArrList.Add(25);
+            myArrList.Add("Hello");
+            myArrList.Add(13.25);
+            myArrList.Add(true);
+            myArrList.Add(new DateTime(1994, 01, 16));
+            myArrList.Add('@');
+            myArrList.Add(25.13f);
+            myArrList.Add("End");
+
+            Console.WriteLine("Array before removing items - {0}", myArrList.Count);
+
+            // remove specific value
+            myArrList.Remove(true);
+
+            // remove specific index
+            myArrList.RemoveAt(2);
+
+            Console.WriteLine("Array after removing items - {0}", myArrList.Count);
+
+            double sum = 0;
+
+            foreach(object obj in myArrList)
+            {
+                Console.WriteLine(obj);
+            }
+
+            foreach (object obj in myArrList)
+            {
+                if(obj is int)
+                {
+                    sum += Convert.ToDouble(obj);
+                }
+                else if (obj is double)
+                {
+                    sum += (double) obj;
+                }
+                else if (obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+
+            Console.WriteLine("The sum is {0}", sum);
+        }
+
+        public static void ParamsExample()
+        {
+            int price = 50;
+            float pi = 3.14f;
+            char at = '@';
+            string book = "The Hobbit";
+
+            Console.WriteLine("price is {0}, pi is {1}, at is {2}", 32, PI, '@');
+            ParamsTestMethod("this", "is", "a", "test", "this", "sentence", "is", "pointless", "but", "it", "could", "go", "forever", "...");
+
+            Console.WriteLine();
+
+            ParamsTestMethodTwo(price, pi, at, book);
+            ParamsTestMethodTwo("Hello", 5.3, '$');
+            ParamsTestMethodTwo();
+
+            int min = MinV2(5, 4, 1, 2, 7, 3, -6);
+
+            Console.WriteLine("Min is {0}", min);
+        }
+
+        public static int MinV2(params int[] numbers)
+        {
+            int min = int.MaxValue;
+
+            foreach (int num in numbers)
+            {
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            
+            return min;
+        }
+
+        public static void ParamsTestMethod(params string[] sentence)
+        {
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                Console.Write(sentence[i] + " ");
+            }
+        }
+        public static void ParamsTestMethodTwo(params object[] objects)
+        {
+            foreach(object obj in objects){
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+        public static void ArrayParameters()
+        {
+            int[] studentsGrades = new int[] { 15, 13, 8, 12, 6, 16 };
+            double averageResult = GetAverage(studentsGrades);
+
+            foreach (int grade in studentsGrades)
+            {
+                Console.WriteLine("Grade: {0}", grade);
+            }
+
+            Console.WriteLine($"The average result is {averageResult}");
+
+            int[] happiness = new int[] { 15, 16, 18, 5, 3 };
+
+            SunIsShining(happiness); // values after method: 17, 18, 20, 7, 5
+
+            foreach (int i in happiness)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        public static void SunIsShining(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i] + 2;
+            }
+        }
+            
+
+        // with method above
+        static double GetAverage(int[] gradesArray)
+        {
+            int size = gradesArray.Length;
+            double average;
+            int sum = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                sum += gradesArray[i];
+            }
+
+            average = (double)sum / size;
+
+            return average;
+        }
+        
+        public static void JaggedArray()
+        {
+            // declare type one
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[5];
+            jaggedArray[1] = new int[3];
+            jaggedArray[2] = new int[2];
+
+            jaggedArray[0] = new int[] { 2, 3, 5, 7, 11 };
+            jaggedArray[1] = new int[] { 1, 2, 3 };
+            jaggedArray[2] = new int[] { 13, 21 };
+
+
+            // alternate way
+            int[][] jaggedArray2 = new int[][]
+            {
+                new int[] { 2, 3, 5, 7, 11 },
+                new int[] { 1, 2, 3 },
+                new int[] { 13, 21 }
+            };
+
+            Console.WriteLine("The value is {0}", jaggedArray2[0][2]);
+
+            for(int i = 0; i < jaggedArray.Length; i++)
+            {
+                
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + " ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
+
+            // friend jagged array
+            Human[][] friends = new Human[][]
+            {
+            new Human[] { new Human("Joan", "Turner"), new Human("Norman", "Turner") },
+            new Human[] { new Human("Carol", "Green"), new Human("Geoff", "Green") },
+            new Human[] { new Human("Julie", "Howard"), new Human("Michael", "Howard") }
+            };
+
+            for (int i = 0; i < friends.Length; i++)
+            {
+
+                for (int j = 0; j < friends[i].Length; j++)
+                {
+                    friends[i][j].IntroduceMyself();
+                }
+                Console.WriteLine("");
+            }
         }
 
         public static void LoopingMatrix()
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # DemoApp
 
-nothing special going on here, just a bunch of basic command line apps created while learning the C# syntax.
+Nothing special going on here, just a bunch of basic command line 
+apps created while learning the C# syntax.
+
+This is not clean of laid out in any specical way. As I work on new 
+concepts I add a method then run that method in the main method.
+
+This results in a messy, unorganised class with lots of methods and 
+sub-classes. But it means I can add to the code without creating a 
+new solution or removing old code.
